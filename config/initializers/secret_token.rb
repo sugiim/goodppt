290a056aab9faaf9e1d4b3088a36caa03331c7d4
sugiim <sugiim250@gmail.com>
2013-05-29 22:49:32 +0900

    new App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goodppt::Application.config.secret_token = '1a94919580de8ae245b7092c97f723bbb276f2a1bd2508610a924da6662a696a894a3dc21d3ea70b67842b1c74839740ff7cf8cca99cd8d06e9534ce9f432f78'
